08c015bcda8a3abdb382f1b20e203926bdb861e6
RudeyPunk <chris@imyth-x.com>
2017-12-15 17:34:24 -0500

    Adding the Simulation Manager but so far nothing has been created

---
+++ Source/RDW_TK3/SimulationManager.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "SimulationManager.h"
+
+SimulationManager::SimulationManager()
+{
+}
+
+SimulationManager::~SimulationManager()
+{
+}
